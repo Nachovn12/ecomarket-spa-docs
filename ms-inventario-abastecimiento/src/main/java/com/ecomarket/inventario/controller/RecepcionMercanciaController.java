62a48a837899d04f50ea18803be8a33bc1a44ec6
Nachovn12 <ig.valeria@duocuc.cl>
2026-05-19 01:08:36 -0400

    fix: agregar hateoas a recepciones de mercancia

--- ms-inventario-abastecimiento/src/main/java/com/ecomarket/inventario/controller/RecepcionMercanciaController.java
+++ ms-inventario-abastecimiento/src/main/java/com/ecomarket/inventario/controller/RecepcionMercanciaController.java
@@ -5,11 +5,16 @@ import com.ecomarket.inventario.dto.RecepcionMercanciaResponseDTO;
 import com.ecomarket.inventario.service.RecepcionMercanciaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api/inventario/recepciones-mercancia")
@@ -18,19 +23,53 @@ public class RecepcionMercanciaController {
     @Autowired
     private RecepcionMercanciaService recepcionService;
 
-    // CA1, CA2, CA3, CA4
     @PostMapping
-    public ResponseEntity<RecepcionMercanciaResponseDTO> registrarRecepcion(@Valid @RequestBody RecepcionMercanciaRequestDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(recepcionService.registrarRecepcion(dto));
+    public ResponseEntity<EntityModel<RecepcionMercanciaResponseDTO>> registrarRecepcion(
+            @Valid @RequestBody RecepcionMercanciaRequestDTO dto) {
+
+        RecepcionMercanciaResponseDTO response = recepcionService.registrarRecepcion(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(toModel(response));
     }
 
     @GetMapping
-    public ResponseEntity<List<RecepcionMercanciaResponseDTO>> listarRecepciones() {
-        return ResponseEntity.ok(recepcionService.listarRecepciones());
+    public ResponseEntity<CollectionModel<EntityModel<RecepcionMercanciaResponseDTO>>> listarRecepciones() {
+        List<EntityModel<RecepcionMercanciaResponseDTO>> recepciones = recepcionService.listarRecepciones()
+                .stream()
+                .map(this::toModel)
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<RecepcionMercanciaResponseDTO>> collection = CollectionModel.of(recepciones);
+        collection.add(linkTo(methodOn(RecepcionMercanciaController.class).listarRecepciones()).withSelfRel());
+
+        return ResponseEntity.ok(collection);
     }
 
     @GetMapping("/pedido/{pedidoId}")
-    public ResponseEntity<List<RecepcionMercanciaResponseDTO>> obtenerPorPedido(@PathVariable Long pedidoId) {
-        return ResponseEntity.ok(recepcionService.obtenerPorPedido(pedidoId));
+    public ResponseEntity<CollectionModel<EntityModel<RecepcionMercanciaResponseDTO>>> obtenerPorPedido(
+            @PathVariable Long pedidoId) {
+
+        List<EntityModel<RecepcionMercanciaResponseDTO>> recepciones = recepcionService.obtenerPorPedido(pedidoId)
+                .stream()
+                .map(this::toModel)
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<RecepcionMercanciaResponseDTO>> collection = CollectionModel.of(recepciones);
+        collection.add(linkTo(methodOn(RecepcionMercanciaController.class).obtenerPorPedido(pedidoId)).withSelfRel());
+        collection.add(linkTo(methodOn(RecepcionMercanciaController.class).listarRecepciones()).withRel("recepciones-mercancia"));
+
+        return ResponseEntity.ok(collection);
+    }
+
+    private EntityModel<RecepcionMercanciaResponseDTO> toModel(RecepcionMercanciaResponseDTO recepcion) {
+        EntityModel<RecepcionMercanciaResponseDTO> model = EntityModel.of(recepcion);
+
+        model.add(linkTo(methodOn(RecepcionMercanciaController.class).listarRecepciones()).withRel("recepciones-mercancia"));
+
+        if (recepcion.getPedidoId() != null) {
+            model.add(linkTo(methodOn(RecepcionMercanciaController.class)
+                    .obtenerPorPedido(recepcion.getPedidoId())).withRel("recepciones-del-pedido"));
+        }
+
+        return model;
     }
 }
